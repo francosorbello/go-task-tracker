56aa0b0cf1ed4af6c16db7cff6b27e261b081bff
francosorbello <franco.sorbelo@gmail.com>
2025-11-03 17:11:12 -0300

    all functions in main return errors now

--- main.go
+++ main.go
@@ -9,76 +9,86 @@ import (
 )
 
 type InvalidArgNumberError struct {
-	commandName string
+	commandName  string
 	expectedArgs int
 	receivedArgs int
 }
 
-func (e *InvalidArgNumberError) Error() string{
-	return fmt.Sprintf("%s command expects %d args, but received %d",e.commandName,e.expectedArgs,e.receivedArgs)
+func (e *InvalidArgNumberError) Error() string {
+	return fmt.Sprintf("%s command expects %d args, but received %d", e.commandName, e.expectedArgs, e.receivedArgs)
 }
 
 func validateArgs(commandName string, expected int, received int) error {
-	if (expected != received) {
-		return &InvalidArgNumberError{commandName,expected, received}
+	if expected != received {
+		return &InvalidArgNumberError{commandName, expected, received}
 	}
 	return nil
 }
 
 type Command struct {
 	argNumber int
-	data  map[string]any
+	data      map[string]any
 }
 
 type Executable interface {
 	Verify(args []string) error
-	Execute(args []string)
+	Execute(args []string) error
 }
 
 type AddCommand Command
 
-func (com *AddCommand) Verify(args []string) error{
-	err := validateArgs("add",com.argNumber,len(args))
-	
-	return err	
+func (com *AddCommand) Verify(args []string) error {
+	err := validateArgs("add", com.argNumber, len(args))
+
+	return err
 }
 
-func (com *AddCommand) Execute(args []string) {
-	task,_ := tasks.AddTask(args[0])
-	fmt.Printf("Task added sucessfully (ID: %d)",task.ID)
+func (com *AddCommand) Execute(args []string) error {
+	task, addTaskErr := tasks.AddTask(args[0])
+	if addTaskErr != nil {
+		return addTaskErr
+	}
+	fmt.Printf("Task added sucessfully (ID: %d)", task.ID)
+	return nil
 }
 
 type UpdateCommand Command
 
-func (com *UpdateCommand) Verify(args []string) error{
-	err := validateArgs("update",com.argNumber,len(args))
-	
-	return err		
+func (com *UpdateCommand) Verify(args []string) error {
+	err := validateArgs("update", com.argNumber, len(args))
+
+	return err
 }
 
-func (com *UpdateCommand) Execute(args []string) {
-	taskId,_ := strconv.Atoi(args[0])
+func (com *UpdateCommand) Execute(args []string) error {
+	taskId, err := strconv.Atoi(args[0])
+	if err != nil {
+		return err
+	}
 	desc := args[1]
-	tasks.UpdateTask(taskId,desc)
+	return tasks.UpdateTask(taskId, desc)
 }
 
 type DeleteCommand Command
 
-func (com *DeleteCommand) Verify(args []string) error{
-	err := validateArgs("delete",com.argNumber,len(args))
-	
-	return err		
+func (com *DeleteCommand) Verify(args []string) error {
+	err := validateArgs("delete", com.argNumber, len(args))
+
+	return err
 }
 
-func (com *DeleteCommand) Execute(args []string) {
-	taskId,_ := strconv.Atoi(args[0])
-	tasks.DeleteTask(taskId)
+func (com *DeleteCommand) Execute(args []string) error {
+	taskId, err := strconv.Atoi(args[0])
+	if err != nil {
+		return err
+	}
+	return tasks.DeleteTask(taskId)
 }
 
 type UpdateStatusCommand Command
 
-func (com *UpdateStatusCommand) Verify(args []string) error{
-	err := validateArgs("update status",com.argNumber,len(args))
+func (com *UpdateStatusCommand) Verify(args []string) error {
+	err := validateArgs("update status", com.argNumber, len(args))
 	if err != nil {
 		return err
 	}
@@ -87,50 +97,58 @@ func (com *UpdateStatusCommand) Verify(args []string) error{
 		return errors.New("command data doesnt have a status entry")
 	}
 	if status > tasks.Done {
-		return fmt.Errorf("invalid status. Received %d, expected %s or %s",status,tasks.InProgress,tasks.Done)
+		return fmt.Errorf("invalid status. Received %d, expected %s or %s", status, tasks.InProgress, tasks.Done)
 	}
 
-	return nil		
+	return nil
 }
 
-func (com *UpdateStatusCommand) Execute(args []string) {
-	taskId,_ := strconv.Atoi(args[0])
+func (com *UpdateStatusCommand) Execute(args []string) error {
+	taskId, err := strconv.Atoi(args[0])
+	if err != nil {
+		return err
+	}
 	status := com.data["status"].(tasks.TaskStatus)
-	
-	tasks.UpdateTaskStatus(taskId,status)
+
+	return tasks.UpdateTaskStatus(taskId, status)
 }
 
 type ListCommand Command
 
-func (com *ListCommand) Verify(args []string) error{
-	err := validateArgs("list",com.argNumber,len(args))
+func (com *ListCommand) Verify(args []string) error {
+	err := validateArgs("list", com.argNumber, len(args))
 	if err != nil {
 		return err
 	}
 
 	if com.argNumber == 1 {
 		statusArg := args[0]
-		if statusArg != "todo" && statusArg !="in-progress" && statusArg != "done" {
-			return fmt.Errorf("invalid action for list command. Expected, todo, in-progress or done. Received %s",statusArg)
+		if statusArg != "todo" && statusArg != "in-progress" && statusArg != "done" {
+			return fmt.Errorf("invalid action for list command. Expected, todo, in-progress or done. Received %s", statusArg)
 		}
-	}	
+	}
 
 	return nil
 }
 
-func (com *ListCommand) Execute(args []string) {
+func (com *ListCommand) Execute(args []string) error {
 	status := -1
 	if com.argNumber == 1 {
 		statusName := args[0]
 		status = int(tasks.StatusNameToValue(statusName))
-		fmt.Println("==",statusName,"==")
+		fmt.Println("==", statusName, "==")
 	} else {
 		fmt.Println("== All tasks ==")
 	}
-	tasks.ListTasks(status)
-	fmt.Println("====")
-}
 
+	err := tasks.ListTasks(status)
+	if err != nil {
+		return err
+	}
+	fmt.Println("====")
+	
+	return nil
+}
 
 func main() {
 	receivedArgs := os.Args[1:]
@@ -142,28 +160,28 @@ func main() {
 	var comm Executable
 	var err error
 	commandData := map[string]any{}
-	
+
 	switch commName {
 	case "add":
-		comm = &AddCommand{1,commandData}
+		comm = &AddCommand{1, commandData}
 	case "update":
-		comm = &UpdateCommand{2,commandData}
+		comm = &UpdateCommand{2, commandData}
 	case "delete":
-		comm = &DeleteCommand{1,commandData}
+		comm = &DeleteCommand{1, commandData}
 	case "mark-in-progress":
-		commandData["status"] = tasks.InProgress 
-		comm = &UpdateStatusCommand{1,commandData}
+		commandData["status"] = tasks.InProgress
+		comm = &UpdateStatusCommand{1, commandData}
 	case "mark-done":
-		commandData["status"] = tasks.Done 
-		comm = &UpdateStatusCommand{1,commandData}
+		commandData["status"] = tasks.Done
+		comm = &UpdateStatusCommand{1, commandData}
 	case "list":
 		if len(receivedArgs) > 1 {
-			comm = &ListCommand{1,commandData}
+			comm = &ListCommand{1, commandData}
 		} else {
-			comm = &ListCommand{0,commandData}
+			comm = &ListCommand{0, commandData}
 		}
 	default:
-		panic(errors.New("no command named "+commName))
+		panic(errors.New("no command named " + commName))
 	}
 
 	err = comm.Verify(receivedArgs[1:])
